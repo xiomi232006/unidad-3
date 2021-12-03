bff8201d8e225eecb5f8da2ed04ddc088252ca7e
xiomi232006 <paredesxiomara816@gmail.com>
2021-12-03 08:25:50 -0500

    Esto es lo avanzado en clase

---
+++ sesion37/sesion37.js
@@ -0,0 +1,52 @@
+
+/*const listaDeNumeros = [2,3,4,7,11];
+console.log(listaDeNumeros[0]);
+console.log(listaDeNumeros[4]);*/
+/*
+let numerosImpares=[1,2,3,5,7];
+cantidad = numerosImpares.length;
+console.log("primera forma:")
+for(i =0; cantidad>i ; i++){
+    console.log(numerosImpares[i]);
+}
+
+console.log("segunda forma:");
+for(const valor of numerosImpares){
+    console.log(valor);
+}*/
+
+//practicando arreglos 
+/*
+const listaDeNumeros = [2,3,5,7,11];
+listaDeNumeros[2]=13;
+console.log(listaDeNumeros[2]);
+*/
+
+//cambiando valores
+/*
+listaDeNumeros=[];
+listaDeNumeros[0]-100
+listaDeNumeros[1]-100;
+listaDeNumeros[2]=-10;
+listaDeNumeros[4]=-5;
+console.log(listaDeNumeros[2])
+*/
+/*
+const mixedArray = [3, "hola mundo", 10.5];
+cantidad = mixedArray.length;
+for (i=0; cantidad>i; i++){
+console.log(mixedArray[i]);
+}*/
+
+let questions= [
+['¿Cual es la capital de Peru?', 'Lima'],
+['¿Cuantos colores tiene el arcoiris?',7],
+['¿Cual es la moneda de Peru?','Sol'],
+];
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+
+console.log(questions[1][1]);
+console.log(questions[2][0]);
+console.log(questions[2][1]);
